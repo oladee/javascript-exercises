b9e12c479262278496cd249bd6db4386a2f3245a
Oladee <deemajor230600@gmail.com>
2024-02-09 23:21:46 +0100

    finished all 12 exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const result = array.reduce((oldest, currentperson)=>{
+        const oldestAge = getpeopleAge(oldest.yearOfBirth,oldest.yearOfDeath)
+        const currentpersonAge = getpeopleAge(currentperson.yearOfBirth,currentperson.yearOfDeath) 
+        return oldestAge < currentpersonAge ? currentperson : oldest
+    })
+    return result
 };
+
+function getpeopleAge(dob, deathday){
+    if(!deathday){
+        deathday = new Date().getFullYear()
+    }
+    return deathday - dob
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
